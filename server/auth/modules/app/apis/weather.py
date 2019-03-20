af8f32bf5a4fba640baa009a0803eef61bee67ee
onyx <bhaskar.droid@gmail.com>
2019-03-20 01:35:36 +0000

    weather external api added

---
+++ server/auth/modules/app/apis/weather.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, jsonify
+import plotly.graph_objs as go
+from plotly.utils import PlotlyJSONEncoder
+import json
+import requests
+import requests_cache
+import os
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from bson.objectid import ObjectId
+from app import app, mongo
+import logger
+
+ROOT_PATH = os.environ.get('ROOT_PATH')
+LOG = logger.get_root_logger(
+    __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
+
+
+requests_cache.install_cache(
+    'weather_api_cache', backend='sqlite', expire_after=36000)
+
+crime_url_template = 'https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={date}'
+categories_url_template = 'https://data.police.uk/api/crime-categories?date={date}'
+
+wather_url = 'http://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22'
+wather_url_apixu = 'http://api.apixu.com/v1/current.json?key={api_key}&q={city}&lang={language}'
+
+
+@app.route('/weatherCity',  methods=['GET'])
+@jwt_required
+def weatherCity():
+
+    respons = requests.get(wather_url)
+
+    if respons.ok:
+        weather = respons.json()
+        return jsonify(weather)
+    else:
+        print(respons.reason)
+
+
+@app.route('/cityWeather',  methods=['GET'])
+@jwt_required
+def weather():
+
+    city = request.args.get('city')
+    language = request.args.get('language')
+    api_key = request.args.get('api_key', '0a90bd51afe74d5889a03447192003')
+
+    weather_url = wather_url_apixu.format(
+        api_key=api_key, city=city, language=language)
+    respons = requests.get(weather_url)
+
+    if respons.ok:
+        weather = respons.json()
+        return jsonify(weather)
+    else:
+        print(respons.reason)
