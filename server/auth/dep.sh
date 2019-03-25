d3b167c93f9c89aa39190367d57debc32dcabe1d
onyx <bhaskar.droid@gmail.com>
2019-03-25 15:13:50 +0000

    shell script updated

--- server/auth/dep.sh
+++ server/auth/dep.sh
@@ -1,3 +1,3 @@
-gcloud config set project 
-gcloud config set compute/zone us-central1-b
 export PROJECT_ID="$(gcloud config get-value project -q)"
+gcloud config set project $PROJECT_ID
+gcloud config set compute/zone us-central1-b
