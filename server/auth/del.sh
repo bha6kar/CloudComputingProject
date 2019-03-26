52b75e4edb89ad8d96c3c2020cf8f23a44e8973e
onyx <bhaskar.droid@gmail.com>
2019-03-26 03:22:58 +0000

    shell script updated

--- server/auth/del.sh
+++ server/auth/del.sh
@@ -1,7 +1,8 @@
+export PROJECT_ID="$(gcloud config get-value project -q)"
 
 kubectl delete service web-app-server
-gcloud container images delete gcr.io/cloudcomputing-235600/exchange-server:v1
+gcloud container images delete gcr.io/$PROJECT_ID/exchange-server:v1
 kubectl delete deployment web-app-server
 
-docker image rm gcr.io/cloudcomputing-235600/exchange-server:v1
+docker image rm gcr.io/$PROJECT_ID/exchange-server:v1
 docker image rm python:3.7
