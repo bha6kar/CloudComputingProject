e288cf4717ba010bfa39e82c6b294d9ee9a88c80
onyx <bhaskar.droid@gmail.com>
2019-03-26 01:23:47 +0000

    shell script updated

--- server/auth/del.sh
+++ server/auth/del.sh
@@ -1,5 +1,6 @@
 
 kubectl delete service web-app-server
-gcloud container images delete gcr.io/cloudcomputing-235600/exchange-server:v1 -y
+gcloud container images delete gcr.io/cloudcomputing-235600/exchange-server:v1
 kubectl delete deployment web-app-server
 
+docker image rm gcr.io/cloudcomputing-235600/exchange-server:v1
--- server/auth/dep.sh
+++ server/auth/dep.sh
@@ -3,7 +3,7 @@ gcloud config set project $PROJECT_ID
 gcloud config set compute/zone us-central1-b
 
 docker build -t gcr.io/${PROJECT_ID}/exchange-server:v1 .
-gcloud auth configure-docker -yes
+gcloud auth configure-docker
 docker push gcr.io/${PROJECT_ID}/exchange-server:v1
 
 kubectl run web-app-server --image=gcr.io/${PROJECT_ID}/exchange-server:v1 --port 8080
