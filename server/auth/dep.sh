4e629aeca6f22953292ecbbcd6ee4c673097b191
onyx <bhaskar.droid@gmail.com>
2019-03-26 00:00:06 +0000

    change to shell deployment script

--- server/auth/dep.sh
+++ server/auth/dep.sh
@@ -1,3 +1,7 @@
 export PROJECT_ID="$(gcloud config get-value project -q)"
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone us-central1-b
+
+docker build -t gcr.io/${PROJECT_ID}/exchange-server:v1 .
+gcloud auth configure-docker
+docker push gcr.io/${PROJECT_ID}/exchange-server:v1
