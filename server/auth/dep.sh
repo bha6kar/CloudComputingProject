bbf2bee65328df0b7c2446e7cc3b1729cccd8df4
onyx <bhaskar.droid@gmail.com>
2019-03-26 01:10:13 +0000

    change to shell deployment script

--- server/auth/dep.sh
+++ server/auth/dep.sh
@@ -7,3 +7,5 @@ gcloud auth configure-docker -yes
 docker push gcr.io/${PROJECT_ID}/exchange-server:v1
 
 kubectl run web-app-server --image=gcr.io/${PROJECT_ID}/exchange-server:v1 --port 8080
+
+kubectl expose deployment web-app-server --type=LoadBalancer --port 80 --target-port 8080
