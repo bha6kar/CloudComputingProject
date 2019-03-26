0c8d9c5511b9dd369550a8eba02d71593fe940c2
onyx <bhaskar.droid@gmail.com>
2019-03-26 00:36:51 +0000

    change to shell deployment script

--- server/auth/dep.sh
+++ server/auth/dep.sh
@@ -3,7 +3,9 @@ gcloud config set project $PROJECT_ID
 gcloud config set compute/zone us-central1-b
 
 docker build -t gcr.io/${PROJECT_ID}/exchange-server:v1 .
-gcloud auth configure-docker
+gcloud auth configure-docker -yes
 docker push gcr.io/${PROJECT_ID}/exchange-server:v1
 
 gcloud container clusters create hello-cluster --num-nodes=3
+
+kubectl run web-app-server --image=gcr.io/${PROJECT_ID}/exchange-server:v1 --port 8080
