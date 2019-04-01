be9b18b9eefa0c99e3104124a43abce3ea9450c2
onyx <bhaskar.droid@gmail.com>
2019-04-01 15:32:50 +0100

    client removed

--- client/src/store/configureReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import login from '../views/reducer';
-import { reducer as form } from 'redux-form';
-
-export default combineReducers({
- login,
- form
-});
